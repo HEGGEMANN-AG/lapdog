5dba68545c95efb2aa654a34d5965718e4790bd9
2ndDerivative <niclas@klugmann.de>
2025-07-22 19:51:44 +0200

    add more trait impls

--- lapdog/src/search/impl_traits.rs
+++ lapdog/src/search/impl_traits.rs
@@ -1,6 +1,7 @@
 use std::{
+    convert::Infallible,
     fmt::Display,
-    num::{NonZeroI8, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroU8, NonZeroU16, NonZeroU32, NonZeroU64},
+    num::{NonZeroI8, NonZeroI16, NonZeroI32, NonZeroI64, NonZeroU8, NonZeroU16, NonZeroU32, NonZeroU64, Saturating},
 };
 
 use crate::search::FromOctetString;
@@ -11,16 +12,34 @@ impl FromOctetString for String {
         String::from_utf8(bytes.to_vec())
     }
 }
+impl FromOctetString for () {
+    type Err = Infallible;
+    fn from_octet_string(_bytes: &[u8]) -> Result<Self, Self::Err> {
+        Ok(())
+    }
+}
+impl<T: FromOctetString> FromOctetString for Box<T> {
+    type Err = T::Err;
+    fn from_octet_string(bytes: &[u8]) -> Result<Self, Self::Err> {
+        Ok(Box::new(T::from_octet_string(bytes)?))
+    }
+}
 macro_rules! from_octet_for_integer {
     ($t:ty) => {
         impl FromOctetString for $t {
-            type Err = ParseNumberError;
+            type Err = ParseIntegerError;
             fn from_octet_string(bytes: &[u8]) -> Result<Self, Self::Err> {
-                let s = str::from_utf8(bytes).map_err(ParseNumberError::Utf8)?;
-                s.parse::<$t>().map_err(ParseNumberError::Parse)
+                let s = str::from_utf8(bytes).map_err(ParseIntegerError::Utf8)?;
+                s.parse::<$t>().map_err(ParseIntegerError::Parse)
             }
         }
     };
+}
+impl<T: FromOctetString> FromOctetString for Saturating<T> {
+    type Err = T::Err;
+    fn from_octet_string(bytes: &[u8]) -> Result<Self, Self::Err> {
+        Ok(Saturating(T::from_octet_string(bytes)?))
+    }
 }
 from_octet_for_integer!(u8);
 from_octet_for_integer!(u16);
@@ -40,11 +59,11 @@ from_octet_for_integer!(NonZeroU32);
 from_octet_for_integer!(NonZeroU64);
 
 #[derive(Clone, Debug)]
-pub enum ParseNumberError {
+pub enum ParseIntegerError {
     Utf8(std::str::Utf8Error),
     Parse(std::num::ParseIntError),
 }
-impl std::error::Error for ParseNumberError {
+impl std::error::Error for ParseIntegerError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
             Self::Parse(p) => Some(p),
@@ -52,7 +71,7 @@ impl std::error::Error for ParseNumberError {
         }
     }
 }
-impl Display for ParseNumberError {
+impl Display for ParseIntegerError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Utf8(u) => write!(f, "server response is not utf-8: {u}"),
