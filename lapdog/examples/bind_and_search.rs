6672128edd2060ac2b39e2910900474e65a1d2f7
2ndDerivative <niclas@klugmann.de>
2025-07-23 11:32:40 +0200

    add example

---
+++ lapdog/examples/bind_and_search.rs
@@ -0,0 +1,38 @@
+use std::net::SocketAddr;
+
+use lapdog::{LdapConnection, search::Entry};
+use rasn_ldap::{AttributeValueAssertion, Filter, SearchRequestDerefAliases, SearchRequestScope};
+
+fn main() {
+    let ip: SocketAddr = std::env::var("LAPDOG_IP").unwrap().parse().unwrap();
+    let unbound = LdapConnection::new(ip).unwrap();
+    let username = std::env::var("LAPDOG_USER").unwrap();
+    let password = std::env::var("LAPDOG_PW").unwrap();
+    let mut bound = unbound.bind_simple(&username, password.as_bytes()).unwrap();
+    let search_filter = Filter::Not(Box::new(Filter::EqualityMatch(AttributeValueAssertion::new(
+        "givenName".into(),
+        b"Steve".to_vec().into(),
+    ))));
+    let search_results = bound
+        .search::<UserEntry>(
+            "OU=Specialists,DC=company,DC=com",
+            SearchRequestScope::WholeSubtree,
+            SearchRequestDerefAliases::DerefAlways,
+            search_filter,
+        )
+        .unwrap();
+    for UserEntry {
+        sam_account_name,
+        company,
+    } in search_results.flatten()
+    {
+        println!("{sam_account_name} works for {company}");
+    }
+}
+
+#[derive(Entry)]
+struct UserEntry {
+    #[lapdog(rename = "sAMAccountName")]
+    sam_account_name: String,
+    company: String,
+}
