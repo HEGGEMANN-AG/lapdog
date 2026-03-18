db60196d58667e4247b99935a2655c7b48dbdc70
Niclas Klugmann <klugmann@heggemann.com>
2026-03-18 17:30:58 +0100

    better attributeValueassertion ergonomics

--- lapdog/src/attribute.rs
+++ lapdog/src/attribute.rs
@@ -11,17 +11,30 @@ pub struct AttributeValueAssertion<'d> {
     pub assertion_value: &'d [u8],
 }
 impl AttributeValueAssertion<'_> {
-    pub fn write_into<W: Write>(&self, w: &mut W) -> std::io::Result<()> {
-        w.write_single_byte(UNIVERSAL_SEQUENCE)?;
+    pub(crate) fn write_into<W: Write>(&self, w: &mut W) -> std::io::Result<()> {
         let mut seq_inner = Vec::new();
-        seq_inner.write_single_byte(OCTET_STRING)?;
-        seq_inner.write_ber_length(self.attribute_desc.len())?;
-        seq_inner.write_all(self.attribute_desc.as_bytes())?;
-        seq_inner.write_single_byte(OCTET_STRING)?;
-        seq_inner.write_ber_length(self.assertion_value.len())?;
-        seq_inner.write_all(self.assertion_value)?;
+        self.write_body_into(&mut seq_inner)?;
+
+        w.write_single_byte(UNIVERSAL_SEQUENCE)?;
         w.write_ber_length(seq_inner.len())?;
         w.write_all(&seq_inner)?;
         Ok(())
+    }
+    pub(crate) fn write_body_into<W: Write>(&self, w: &mut W) -> std::io::Result<()> {
+        w.write_single_byte(OCTET_STRING)?;
+        w.write_ber_length(self.attribute_desc.len())?;
+        w.write_all(self.attribute_desc.as_bytes())?;
+        w.write_single_byte(OCTET_STRING)?;
+        w.write_ber_length(self.assertion_value.len())?;
+        w.write_all(self.assertion_value)?;
+        Ok(())
+    }
+}
+impl<'d> AttributeValueAssertion<'d> {
+    pub const fn new(attribute_desc: &'d str, assertion_value: &'d [u8]) -> Self {
+        Self {
+            attribute_desc,
+            assertion_value,
+        }
     }
 }
